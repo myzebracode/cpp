ebc6a7ac77f6539bf3ff922926a68efbe768ccfb
myluis <ibhluis@gmail.com>
2025-08-20 16:14:54 +0100

    C Programming File for In-memory database

---
+++ myimd.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include "sqlite/sqlite3.c"
+
+int main() {
+
+sqlite3 *db;
+char *err_msg = 0;
+
+//Here, we open our in-memory database
+int conn = sqlite3_open(":memory:", &db);
+
+//Do a database connection check
+if(conn != SQLITE_OK){
+	fprintf(stderr, "Unable to connect to database: %s \n", sqlite3_errmsg(db));
+
+	sqlite3_close(db);
+
+	return 1;
+}
+
+//Now, create a table and insert data into it
+char *sql_qry = "CREATE TABLE contacts(id INTEGER PRIMARY KEY, name TEXT, course TEXT);"
+"INSERT INTO contacts(name, course) VALUES ('Ed', 'Geography');"
+"INSERT INTO contacts(name, course) VALUES ('Becky', 'Law');"
+"INSERT INTO contacts(name, course) VALUES ('Frank', 'Mathematics');"
+"INSERT INTO contacts(name, course) VALUES ('Rodney', 'Physics');"
+"INSERT INTO contacts(name, course) VALUES ('Jeff', 'Biology');"
+"INSERT INTO contacts(name, course) VALUES ('Rita', 'Agricultural Science');"
+"INSERT INTO contacts(name, course) VALUES ('Leona', 'Economics');"
+"INSERT INTO contacts(name, course) VALUES ('Erika', 'Artificial Intelligence');";
+
+//Execute the query
+conn = sqlite3_exec(db, sql_qry, 0, 0, &err_msg);
+
+//Do a check for table creation
+if(conn != SQLITE_OK){
+	fprintf(stderr, "Failed to create table \n");
+	fprintf(stderr, "SQL error: %s \n", err_msg);
+	sqlite3_free(err_msg);
+} else {
+	fprintf(stdout, "Table created successfully \n");
+}
+
+//Now, let's create a variable to print out last inserted rowid
+int last_id = sqlite3_last_insert_rowid(db);
+printf("The id of last inserted row is: %d \n", last_id);
+
+//Let's create a variable to print out previous id to last rowid
+int prev_id = sqlite3_last_insert_rowid(db) - 1;
+printf("The id before last inserted row is: %d \n", prev_id);
+
+//Now, create a variable to print out next rowid
+int next_id = sqlite3_last_insert_rowid(db) + 1;
+printf("The id of next inserted row will be: %d \n", next_id);
+
+//Close the database
+sqlite3_close(db);
+
+	return 0;
+}
